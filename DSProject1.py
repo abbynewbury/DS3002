13738c42570a2a50b8c755f645c47c16e998b8d6
abbynewbury <amn5ye@virginia.edu>
2021-11-07 12:11:54 -0500

    Update DSProject1.py
    adding more informative errors

--- DSProject1.py
+++ DSProject1.py
@@ -5,6 +5,8 @@
 import requests
 import pandas as pd
 import json
+import sys
+import os.path
 
 
 # normals daily: https://www.ncdc.noaa.gov/cdo-web/datasets
@@ -23,10 +25,15 @@ csv_file.write(url_content)
 csv_file.close()
 
 
-#reading in newly written csv,
-df = pd.read_csv ('normaldata.csv')
-print("First 5 rows of csv file:")
-print(df.head())
+#reading in newly written csv and informative error if csv file path incorrect
+path = 'normaldata.csv'
+file_exists = os.path.exists(path)
+if file_exists:
+    # if it works, print first 5 rows of csv file
+    df = pd.read_csv(path)
+    print(df.head())
+else:
+    sys.exit('The file path for the csv was: ' + path + '. This path resulted in a file not found error.')
 
 "Generate brief summary of data file:"
 record_count = len(df)
@@ -37,18 +44,27 @@ print("Record count: " + str(record_count))
 
 
 "Modify number of columns from source to destination by reducing columns"
-# dropping station name b/c only one unique value for column
+# dropping station name b/c only one unique value for both columns
 # print(df['STATION_NAME'].unique())
-df.drop('STATION_NAME', inplace=True, axis=1)
+# including informative error if column name not found
+dropped_column = 'STATION_NAME'
+found = df.get(dropped_column)
+if found.empty:
+    sys.exit("The column to be dropped, " + dropped_column + ", was not found.")
+else:
+    df.drop(dropped_column, inplace=True, axis=1)
 
 "Convert to json and Write to disk"
 df = df.to_json('normaldata.json')
 
 
-# checking json file written to disk correctly
+# checking json file written to disk correctly, if not, producing informative error
 with open('normaldata.json','r') as json_file:
     loaded_json = json.load(json_file)
-    print("Keys written to json file:")
+    key_count = 0
     for key,value in loaded_json.items():
         print(key)
+        key_count += 1
+    if key_count != 8:
+        print("Expected json file to have 8 columns written to it, instead " + str(key_count) + " columns written.")
     json_file.close()
